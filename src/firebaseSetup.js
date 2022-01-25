11a91b529075eab48785cc42c661da28ee2d5c78
Erin <erin.braz@gmail.com>
2022-01-25 18:58:11 -0500

    --

--- src/firebaseSetup.js
+++ src/firebaseSetup.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyASs1QYUOCfAzb6MBr3DhX0zrSMtTQtN44",
-  authDomain: "binge-fest.firebaseapp.com",
-  projectId: "binge-fest",
-  storageBucket: "binge-fest.appspot.com",
-  messagingSenderId: "562103674808",
-  appId: "1:562103674808:web:008ccd1c9ad4f80aa6514d"
-};
-
-// Initialize Firebase
-const bingeFestApp = initializeApp(firebaseConfig);
-
-export default bingeFestApp;
-=======
-// firebaseSetup.js - src folder
-
 import { initializeApp } from "firebase/app";
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -52,4 +29,3 @@ const firebaseConfig = {
 const bingeFest = initializeApp(firebaseConfig);
 
 export default bingeFest;
->>>>>>> tv-page
