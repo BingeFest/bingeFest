54fba74b56dc5c068c76f9f2a847923f9465f563
Erin <erin.braz@gmail.com>
2022-01-27 20:36:18 -0500

    adding component stylesheet for favourites list

--- src/components/FavouritesList.js
+++ src/components/FavouritesList.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { getDatabase, ref, onValue, remove } from 'firebase/database';
 import bingeFestApp from '../firebaseSetup';
+import './favouriteslist.css'
 
 
 function FavouritesList() {
